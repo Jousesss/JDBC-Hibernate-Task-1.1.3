512812bd8249ee6fa84b30d12b59df7b592c4d5f
Aleksey Boorilov <aleksei.burilov@gmail.com>
2024-05-02 09:46:49 +0500

    changed two methods and a little bit of util class

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,35 +2,21 @@ package jm.task.core.jdbc.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
-
-    private static final String dataBaseUrl = "url";
-    private static final String dataBaseUser = "name";
-    private static final String dataBasePassword = "password";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String DB_USERNAME = "postgres";
+    private static final String DB_PASSWORD = "1008";
 
     public static Connection getConnection() {
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection(dataBaseUrl,dataBaseUser,dataBasePassword);
+            connection = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
             Class.forName("org.postgresql.Driver");
         } catch (Exception e) {
             System.err.println("Ошибка получения connection: ");
             e.printStackTrace();
         }
         return connection;
-    }
-
-    public static Statement getStatement() {
-        Statement statement = null;
-        try {
-            statement = getConnection().createStatement();
-        } catch (Exception e) {
-            System.err.println("Ошибка получения statement: ");
-            e.printStackTrace();
-        }
-        return statement;
     }
 }
